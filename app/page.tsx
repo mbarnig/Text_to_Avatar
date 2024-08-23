49896700f584d999484e4a4c5b86665f821d2f9b
Marco BARNIG <mbarnig@pt.lu>
2024-08-23 16:35:04 +0100

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import InteractiveAvatar from "@/components/InteractiveAvatar";
+
+export default function App() {
+  return (
+     <div className="w-screen h-screen flex flex-col">
+       <div className="w-[900px] flex flex-col items-start justify-start gap-5 mx-auto pt-4 pb-20">
+          <InteractiveAvatar />
+       </div>
+     </div>
+  );
+} 
